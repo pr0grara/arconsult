671f17ffbf250ec4046674f96a162fd0a3a367ed
pr0grara <azbaghda@gmail.com>
2020-12-29 19:10:56 -0800

    William To

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+
 app.get("/", (req, res) => {
-  // res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"))
+  // res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   res.send("welcome to the backend");
 })
 
