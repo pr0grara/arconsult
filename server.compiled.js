ae0a37df14db4c59d69ab5c5d838e04c00c1c133
pr0grara <azbaghda@gmail.com>
2021-01-02 15:37:51 -0800

    forgot to build SMH

--- server.compiled.js
+++ server.compiled.js
@@ -21,7 +21,7 @@ app.use("/api/sms", _sms["default"]);
 app.get("/*", function (req, res) {
   res.sendFile(_path["default"].join(__dirname, "../client/build/index.html"));
 });
-var port = 9000;
-app.listen(port, function () {
-  return console.log("app listening on port ".concat(port));
+var PORT = process.env.PORT || 9000;
+app.listen(PORT, function () {
+  return console.log("app listening on port ".concat(PORT));
 });
