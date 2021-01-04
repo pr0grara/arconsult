c133cbfabd56d458618b575efc496c6c2b5bdb11
pr0grara <azbaghda@gmail.com>
2021-01-03 22:44:48 -0800

    proxy bug troubleshoot continued

--- server.compiled.js
+++ server.compiled.js
@@ -8,20 +8,16 @@ var _sms = _interopRequireDefault(require("./routes/api/sms"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var answers = require("./routes/api/sms");
-
 var app = (0, _express["default"])();
-app.use(_express["default"]["static"](_path["default"].join(__dirname, "client", "build"))); //magical shit right here dawg
+var PORT = process.env.PORT || 9000; // app.use(express.static(path.join(__dirname, "client", "build"))); //magical shit right here dawg
 
 app.get("/", function (req, res) {
-  // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  res.send("welcome to the backend");
+  res.send("welcome to the backend on port ".concat(PORT));
 });
-app.use("/api/sms", _sms["default"]); // app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-
-var PORT = process.env.PORT || 9000;
+app.use("/api/sms", _sms["default"]);
+app.get("/*", function (req, res) {
+  res.sendFile(_path["default"].join(__dirname, "../client/build/index.html"));
+});
 app.listen(PORT, function () {
   return console.log("app listening on port ".concat(PORT));
 });
--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 import express from "express";
 import path from "path";
 import sms from './routes/api/sms'
-const answers = require("./routes/api/sms");
-
 
 const app = express();
+const PORT = process.env.PORT || 9000;
 
-app.use(express.static(path.join(__dirname, "client", "build"))); //magical shit right here dawg
-
+// app.use(express.static(path.join(__dirname, "client", "build"))); //magical shit right here dawg
 
 app.get("/", (req, res) => {
-  // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  res.send("welcome to the backend");
+  res.send(`welcome to the backend on port ${PORT}`);
 })
 
 app.use("/api/sms", sms)
 
-// app.get("/*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
-const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
