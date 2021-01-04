8c20662f4e3bbf410cc4b30dc80a837a90c30d98
pr0grara <azbaghda@gmail.com>
2021-01-03 23:38:38 -0800

    proxy bug troubleshoot continued 15

--- server.compiled.js
+++ server.compiled.js
@@ -8,6 +8,10 @@ var _sms = _interopRequireDefault(require("./routes/api/sms"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+require("dotenv").config({
+  path: _path["default"].resolve(__dirname, "./.env")
+});
+
 var app = (0, _express["default"])();
 var PORT = process.env.PORT || 9000; // const PORT = 9000;
 // app.use(express.static(path.join(__dirname, "client", "build"))); //magical shit right here dawg
@@ -16,6 +20,20 @@ app.get("/", function (req, res) {
   res.send("welcome to the backend on port ".concat(PORT));
 });
 app.use("/api/sms", _sms["default"]);
+app.get("/sms/send", function () {
+  var client = require("twilio")(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+  var standardMsg = "hello";
+  client.messages.create({
+    body: standardMsg,
+    from: "+13158030650",
+    to: "+19252553225"
+  })["catch"](function (err) {
+    return console.log(err);
+  }).then(function (message) {
+    return console.log(message.sid);
+  });
+});
 app.get("/*", function (req, res) {
   res.sendFile(_path["default"].join(__dirname, "../client/build/index.html"));
 });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import sms from './routes/api/sms'
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
 const app = express();
 const PORT = process.env.PORT || 9000;
@@ -13,6 +14,22 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/sms", sms)
+
+app.get("/sms/send", () => {
+  const client = require("twilio")(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
+  );
+  var standardMsg = `hello`;
+  client.messages
+    .create({
+      body: standardMsg,
+      from: "+13158030650",
+      to: "+19252553225",
+    })
+    .catch((err) => console.log(err))
+    .then((message) => console.log(message.sid));
+})
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
