2bed2036ee91a7bf941005b6c83df0c8dcc3383a
pr0grara <azbaghda@gmail.com>
2021-01-03 23:16:47 -0800

    proxy bug troubleshoot continued 11

--- client/src/util/api/sms_api_util.js
+++ client/src/util/api/sms_api_util.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const sendSMS = (data) => {
   // debugger
-  return axios.post(`/var/app/current/routes/api/sms/send`, data);
+  return axios.post(`/api/sms/send`, data);
 };
